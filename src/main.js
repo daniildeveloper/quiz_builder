8a69d0082cf6162cdfb9fa05e2c9599fd9548da3
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-02-11 18:04:35 +0600

    Find other new component and imported all it

--- src/main.js
+++ src/main.js
@@ -1,45 +1,42 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'es6-promise/auto';
-import Vue from 'vue';
-import Buefy from 'buefy';
-import 'buefy/lib/buefy.css';
-import store from './store';
+import Vue from 'vue'
+import App from './App'
+import I18Next from '@/plugins/i18next'
+import Axios from '@/plugins/axios'
+import Analytic from '@/plugins/analytic'
+import Focus from '@/plugins/focus'
+import translations from '@/locales/index.js'
+import store from '@/store'
+import Auth from '@/plugins/auth'
+import router from './router'
+import HelpTooltip from '@/components/HelpTooltip'
+import CSwitch from '@/components/Switch'
 
-// Plugins
-import AxiosPlugin from './plugins/axios';
-import HorizontalScroll from './plugins/horizontalScroll';
-// import Analytic from './plugins/analytic';
-import I18Next from './plugins/i18next';
-import translations from './locales';
+import Tooltip from 'vue-directive-tooltip'
+import 'vue-directive-tooltip/css/index.css'
+import { Vue2Dragula } from 'vue2-dragula'
 
 // Components
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-
-// Npm
-import Dragscroll from 'vue-dragscroll';
-import App from './App';
-import router from './router';
-
-/*
-const domain = location.hash.substring(1);
-if (domain) {
-  document.domain = domain;
-}
-*/
-
-Vue.use(Buefy);
-Vue.use(AxiosPlugin);
-Vue.use(Dragscroll);
-Vue.use(HorizontalScroll);
-// Vue.use(Analytic, {});
-Vue.use(I18Next, { translations, lang: store.state.lang });
-Vue.use(VueAwesomeSwiper);
-
-Vue.config.productionTip = false;
+import BootstrapVue from 'bootstrap-vue'
 
 // Styles
-require('swiper/dist/css/swiper.css');
+import 'dragula/dist/dragula.css'
+
+Vue.use(I18Next, { translations, lang: store.state.lang })
+Vue.use(Axios)
+Vue.use(Auth, { router })
+Vue.use(Focus)
+Vue.use(Analytic, { router })
+Vue.use(Vue2Dragula)
+Vue.use(Tooltip)
+Vue.use(BootstrapVue)
+
+Vue.component('alert', require('vue-strap').alert)
+Vue.component('dropdown', require('vue-strap').dropdown)
+Vue.component('modal', require('vue-strap').modal)
+Vue.component('HelpTooltip', HelpTooltip)
+Vue.component('CSwitch', CSwitch)
 
 /* eslint-disable no-new */
 new Vue({
@@ -47,5 +44,10 @@ new Vue({
   store,
   router,
   template: '<App/>',
-  components: { App },
-});
+  components: { App }
+})
+
+
+
+// WEBPACK FOOTER //
+// ./src/main.js
